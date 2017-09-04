7b7a0695015301573196f2364828f893a87b8aa1
Jordan Micah Bennett <jordanmicahbennett@gmail.com>
2017-09-04 02:19:16 -0500

    Update hierarchical_causal_fabric_macroscale-construct.m

--- source code/hierarchical_causal_fabric_macroscale-construct.m
+++ source code/hierarchical_causal_fabric_macroscale-construct.m
@@ -1,7 +1,6 @@
 %% Author ~ Christopher Lu
-%% Adaptor ~ Jordan Micah Bennett ~ Thought Curvature Hypothesis
-%% Adaptor ~ Adaptation Designation ~ Thought Curvature Abstraction : "Causal Neural Perturbation Curvature ( Causal Neural Manifold ( Causal Neural Atom ) )"
-%% Adaptor ~ Adaptation Intent : The encodement of curvature par MESOSCALE/MACROSCALE {η} abstraction EXPRESSION, in the Belmanian regime. Therein, I shall derive strictly non-intemperate fluid-particle|particle-particle interaction sequences, betwixt some reasonable bound in Mach number M terms...
+%% Editor ~ Jordan Micah Bennett ~ Thought Curvature Hypothesis
+%% Editor ~ Adaptation Intent : The encodement of curvature par MESOSCALE/MACROSCALE {η} abstraction EXPRESSION, in the Belmanian regime. Therein, I shall derive strictly non-intemperate fluid-particle|particle-particle interaction sequences, betwixt some reasonable bound in Mach number M terms...
 
 
 %% Defined Constants
